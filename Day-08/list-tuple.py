2ce477d04e03beb1cd91f9bd9b33f7b8ea8028a6
chanduooo <142959302+chanduooo@users.noreply.github.com>
2024-06-02 09:16:17 +0530

    Create list-tuple.py

---
+++ Day-08/list-tuple.py
@@ -0,0 +1,25 @@
+prasad = [10,20,30,50,60,70]
+siva = ["salary","payslips","empid"]
+print(prasad) # print the list of elements
+
+print(len(prasad)) # print the lenth of the elements in the list
+
+# print the sorting order in the list
+prasad.sort()
+print(prasad)
+
+# adding element in the list
+prasad.append(80)
+print(prasad)
+
+# remove element frm the list
+prasad.remove(80)
+print(prasad)
+
+
+# concating element in the list
+print(siva[0] + "--" + siva[1])
+
+# slicing element in the list
+siva1 = [siva[0:3]]
+print(siva1)
